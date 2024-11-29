a205959db9e6cd8de47bed1c26969f11100a85b9
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-11-28 22:36:53 -0500

    create client, db, collection and export collection

--- mean-stack-example/server/src/database.ts
+++ mean-stack-example/server/src/database.ts
@@ -5,3 +5,17 @@ import {Employee} from "./employee"; //import Employee interface from employee f
 export const collections: {
     employees?: mongodb.Collection<Employee>; //these employees are of type Employee
 } = {};
+
+export async function connectToDatabase(uri:string){
+    const client = new mongodb.MongoClient(uri);
+    await client.connect(); //connect to Cluster
+
+    const db = client.db("meanStackExample"); //collect database titled meanStackExample from cluster
+    //if database not there, it will be created
+    await applySchemaValidation(db);//ensure all documents follow Employee interface, GOOD PRACTICE
+
+    const employeesCollection = db.collection<Employee>("employees");//creating employee collection on our database called employees
+    collections.employees = employeesCollection; //add collection to collections which is being exported
+
+
+}
