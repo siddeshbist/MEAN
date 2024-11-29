d415a3a87db327f70e21b7a667eac33c55c88acc
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-11-28 23:44:10 -0500

    added schema validation

--- mean-stack-example/server/src/database.ts
+++ mean-stack-example/server/src/database.ts
@@ -18,8 +18,31 @@ export async function connectToDatabase(uri:string){
     collections.employees = employeesCollection; //add collection to collections which is being exported
 }
 
-function applySchemaValidation(db: mongodb.Db){
+// Update our existing collection with JSON schema validation so we know our documents will always match the shape of our Employee model, even if added elsewhere.
+async function applySchemaValidation(db: mongodb.Db) {
     const jsonSchema = {
-        
-    }
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["name", "position", "level"],
+            additionalProperties: false,
+            properties: {
+                _id: {},
+                name: {
+                    bsonType: "string",
+                    description: "'name' is required and is a string",
+                },
+                position: {
+                    bsonType: "string",
+                    description: "'position' is required and is a string",
+                    minLength: 5
+                },
+                level: {
+                    bsonType: "string",
+                    description: "'level' is required and is one of 'junior', 'mid', or 'senior'",
+                    enum: ["junior", "mid", "senior"],
+                },
+            },
+        },
+    };
 }
+
