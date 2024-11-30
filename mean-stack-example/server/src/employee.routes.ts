eca1ab895158f69ee4208ebf67d5967150ad6394
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-11-30 18:10:00 -0500

    added await inside try block

--- mean-stack-example/server/src/employee.routes.ts
+++ mean-stack-example/server/src/employee.routes.ts
@@ -9,7 +9,8 @@ employeeRouter.use(express.json());
 //get route
 employeeRouter.get("/",(req,res)=>{
     try{
-
+        const employees = await collections?.employees?.find({}).toArray();
+        res.status(200).send(employees)
     }catch(error){
         res.status(500).send(error instanceof Error? error.message:"Unknown errror");
     }
