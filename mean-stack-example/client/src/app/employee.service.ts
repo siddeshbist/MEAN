4d988489d5529ce85e0dff92a60be5bb37b567be
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-12-04 23:06:35 -0500

    added location of server

--- mean-stack-example/client/src/app/employee.service.ts
+++ mean-stack-example/client/src/app/employee.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
-  constructor() { }
+  private url = 'http://localhost:5200'; //server running here
+  constructor(private httpClient: HttpClient) { }
 }
