70154d78487db02561faaec298d71318e32201ec
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-12-04 23:18:34 -0500

    started function to perform GET request to endpoint and retreive employees

--- mean-stack-example/client/src/app/employee.service.ts
+++ mean-stack-example/client/src/app/employee.service.ts
@@ -8,4 +8,18 @@ import { Employee } from './employee';
 export class EmployeeService {
   private url = 'http://localhost:5200'; //server running here
   constructor(private httpClient: HttpClient) { }
+
+  private refreshEmployees(){
+    this.httpClient.get<Employee[]>(`${this.url}/employees`)
+  }
 }
+
+//this.httpClient.get
+//method from Angular HttpClient service, which is used to make HTTP GET requests
+//fetches data from a server using provided URL
+
+//<Employee[]>
+//This is a TypeScript generic type that specifies the expected shape of the response
+//tells the compiler that the response will be an array of objects matching the Employee interface or class
+
+//the method send a GET request to URL and retreives array of objects that are type Employee from the server
