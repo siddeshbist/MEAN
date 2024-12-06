b18aae0e45b8a3b2a3d10a24b8bd3c30e6941d88
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-12-05 21:52:43 -0500

    added explanation on signals and refreshEmployees() method

--- mean-stack-example/client/src/app/employee.service.ts
+++ mean-stack-example/client/src/app/employee.service.ts
@@ -7,19 +7,46 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
   private url = 'http://localhost:5200'; //server running here
+  //2 signals which are used to handle reactive states
+  //Signals provide a clean and efficient way to manage state in Angular,
+  //automatically triggering updates in the UI when the state changes
+  
+  //Creates a signal named employees$ that holds an array of Employee objects
+  //The type <Employee[]> indicates this signal will store an array of employees
+  //Initially the signal is set to an empty array ([])
+  //Purpose is to track and manage a list of employees reactively. Changes to the list will automatically trigger updates wherever signal is being used
+  employees$ = signal<Employee[]>([]);
+  
+  //This creates a signal named employee$ that holds a single Employee object
+  //The type <Employee> indicates that this signal will store a single Employee
+  //Initially, it is set to an empty object cast as Employee using '{} as Employee' even if 
+  //it doesn't yet conform to the Employee type structure
+  //Typically used to track and manage a single employees data reactively such as the 
+  //currently selected employee
+  employee$ = signal<Employee>({} as Employee); 
+
+//1.Reactive State Management
+//These signals can be updated(set,update) and read in a reactive way. Whenever the state of employees$ or employee$ changes
+//the angular components or templates using them will automatically update
+
+//constructor(dependency:DependencyType)
   constructor(private httpClient: HttpClient) { }
 
+  //private method so only accessed within same class
+  //fetch latest list of employees from an API and update a reactive signal (employees$) with the new data
+  //The <Employee[]> specifies endpoint will return back array containing employee objects
+  //.subscribe subscribes to the observable returned by httpClient.get to handle the response when the data arrives
+  //the employees parameter in the callback function holds the array of Employee objects
+  //the set method of the signal replaces the current value with the new data(employees)
+  
   private refreshEmployees(){
     this.httpClient.get<Employee[]>(`${this.url}/employees`)
+    .subscribe(employees => {
+      this.employees$.set(employees);
+    });
   }
+
+
 }
 
-//this.httpClient.get
-//method from Angular HttpClient service, which is used to make HTTP GET requests
-//fetches data from a server using provided URL
 
-//<Employee[]>
-//This is a TypeScript generic type that specifies the expected shape of the response
-//tells the compiler that the response will be an array of objects matching the Employee interface or class
-
-//the method send a GET request to URL and retreives array of objects that are type Employee from the server
