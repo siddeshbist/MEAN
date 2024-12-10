a3b8c115bbca7a5229540691461cc9c9782f6adc
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-12-09 22:39:43 -0500

    added add-employee component

---
+++ mean-stack-example/client/src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-employee',
+  imports: [],
+  template: `
+    <p>
+      add-employee works!
+    </p>
+  `,
+  styles: ``
+})
+export class AddEmployeeComponent {
+
+}
