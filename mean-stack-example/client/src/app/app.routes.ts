1d6d51340f94aa1e2bf64732a19bc6a5eaa49ea0
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-12-10 09:24:26 -0500

    AddEmployeeComponent and EditEmployeeComponent to app routes

--- mean-stack-example/client/src/app/app.routes.ts
+++ mean-stack-example/client/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+
 
 export const routes: Routes = [
-    {path: '',component: EmployeesListComponent, title:'Employees List'},
+    {path: '',component: EmployeesListComponent, title:'Employees List'},//root path
+    {path:"new", component: AddEmployeeComponent},
+    {path: "edit/:id", component: EditEmployeeComponent},
 ];
