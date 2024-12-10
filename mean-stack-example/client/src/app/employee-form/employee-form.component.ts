d5e98906c2a34223b768c49a0cd5a61f23718c05
Siddesh Bist <siddeshbist@Siddeshs-MacBook-Air.local>
2024-12-09 22:26:38 -0500

    added employee form component

---
+++ mean-stack-example/client/src/app/employee-form/employee-form.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-form',
+  imports: [],
+  template: `
+    <p>
+      employee-form works!
+    </p>
+  `,
+  styles: ``
+})
+export class EmployeeFormComponent {
+
+}
